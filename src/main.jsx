c23659ca31b7b8f63c5aba4176be57dd33875dca
narayaabinnadhkari <narayanabin223@gmail.com>
2023-07-21 23:37:50 +0545

    Project setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,11 @@
-import { QueryClient, QueryClientProvider } from "react-query";
 import React from "react";
-
 import ReactDOM from "react-dom/client";
+
 import App from "./App.jsx";
 import "./index.css";
 
-const queryClient = new QueryClient();
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 );
